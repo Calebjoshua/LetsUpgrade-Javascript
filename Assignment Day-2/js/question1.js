8ab6fca90370e1bc7ea00278275faf0b3e6b2b06
Calebjoshua <68387493+Calebjoshua@users.noreply.github.com>
2020-09-02 19:00:33 +0530

    Add files via upload

---
+++ Assignment Day-2/js/question1.js
@@ -0,0 +1,3 @@
+//to find c
+let name="caleb joshua";
+console.log(name.charAt(1));
---
+++ Assignment Day-2/js/question5.js
@@ -0,0 +1,4 @@
+let names1=["caleb","joshua","john","cena","virat","kohli"];
+for(let i=names1.length-1;i>=0;i--){
+    console.log(names1[i]);
+}
